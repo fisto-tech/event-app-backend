1a071c9911bc5eff4a8c94f3f0b7ee45f78c7167
sham-Developer <shamamal622@gmail.com>
2026-03-11 16:02:06 +0530

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://vnbmlkd2-5173.inc1.devtunnels.ms/',
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
